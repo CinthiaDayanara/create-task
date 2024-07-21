b58536ce4e70f4be54c059a55778b7876ac7de45
SaraSaragozin <saralizeth0711@gmail.com>
2024-07-21 18:17:54 -0500

    Add CORS for conexion

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const taskRoutes = require('./routes/taskRoutes');
 const app = express();
 const port = 3008;
@@ -8,6 +9,12 @@ const port = 3008;
 mongoose.connect('mongodb://localhost:27017/task_db', {
     
 });
+
+
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE',
+}));
 
 
 app.use(express.json());
